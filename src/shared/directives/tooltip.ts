1be69892426e7bdd275e97469d106dc43d13c968
timon <timonsus@gmail.com>
2023-10-28 00:34:23 +0500

    tooltip

---
+++ src/shared/directives/tooltip.ts
@@ -0,0 +1,52 @@
+import { Directive } from 'nuxt/dist/app/compat/capi'; 
+import { computePosition, ComputePositionConfig, flip, offset } from '@floating-ui/dom';
+import { TooltipBinding } from './types';
+
+type Options = {
+  open: boolean,
+} & ComputePositionConfig
+
+const compute = async (el: HTMLElement, tooltipEl: HTMLElement, options: Options) => {
+  const ctx = await computePosition(el, tooltipEl, {
+    placement: options.placement,
+    strategy: options.strategy,
+    middleware: options.middleware,
+  });
+  tooltipEl.style.left = `${ctx.x}px`;
+  tooltipEl.style.top = `${ctx.y}px`;
+}
+
+/**
+ * tooltip на основе floating-ui
+ * @param el 
+ * @param param1 
+ * 
+ * @example 
+ * <div ref="tooltipRef">tooltip<div>
+ * <button v-tooltip:right.fixed="tooltipRef"><button>
+ */
+const tooltip: Directive<HTMLElement, HTMLElement> = (
+  el: HTMLElement,
+  {
+    value: tooltipEl,
+    modifiers: {fixed},
+    arg: placement
+  }: TooltipBinding
+) => {
+  if (!tooltipEl) return;
+  
+  el.addEventListener('mouseenter', () => {
+    compute(el, tooltipEl, {
+      placement,
+      strategy: fixed ? 'fixed' : 'absolute',
+      middleware: [flip(), offset(45)],
+      open: true,
+    });
+    tooltipEl.style.visibility = 'visible';
+  });
+  el.addEventListener('mouseleave', () => {
+    tooltipEl.style.visibility = 'hidden';
+  });
+};
+
+export default tooltip;
