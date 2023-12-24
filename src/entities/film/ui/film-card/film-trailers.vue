17818220f01da0358b66848527f66e7250dcbfd8
timon <timonsus@gmail.com>
2023-12-25 02:12:29 +0500

    hide trailers

--- src/entities/film/ui/film-card/film-trailers.vue
+++ src/entities/film/ui/film-card/film-trailers.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { YoutubeIframe } from '@vue-youtube/component';
-import { AppText } from '~/shared/ui';
+import { AppText, AppIcon, BadgeWW } from '~/shared/ui';
 import { FilmService } from './service';
 
 const service = inject('filmService') as FilmService;
 const { trailers } = service;
+const isShow = ref(false);
 </script>
 
 <template>
   <div>
-    <AppText variant="h6" class="mt-6">Трейлеры</AppText>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 flex-wrap">
+    <AppText variant="h6" class="mt-6">Трейлеры
+      <BadgeWW class="text-sm inline-flex cursor-pointer ml-2 align-text-top" color="warning" @click="isShow = !isShow">
+        <AppIcon v-show="!isShow" class="icon" title="Добавлен в список" icon-name="chevron-down" />
+        <AppIcon v-show="isShow" class="icon" title="Добавлен в список" icon-name="chevron-up" />
+      </BadgeWW>
+    </AppText>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 flex-wrap" v-show="isShow">
       <AppText v-if="!trailers.length" variant="simple" class="mt-6">Трейлеры не найдены</AppText>
       <YoutubeIframe
         v-for="trailer in trailers"
