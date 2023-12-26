8825d711fe74829d678474dbc4790954cab926b5
timon <timonsus@gmail.com>
2023-12-26 23:28:54 +0500

    dnd film list

--- src/shared/ui/app-select-list.vue
+++ src/shared/ui/app-select-list.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-const props = defineProps<{list: any[]; keyTitle?: string}>();
+const props = defineProps<{list?: any[]; keyTitle?: string}>();
 const emit = defineEmits<{(e: 'selected', film: any): void}>();
 
-type ListItemType = typeof props.list[number];
-const key = props.keyTitle as keyof ListItemType || 'title';
+// type ListItemType = typeof props.list[number];
+const key = props.keyTitle || 'title';
 </script>
 
 <template>
   <ul class="flex flex-col overflow-auto">
-    <template v-if="$slots.item">
-      <slot v-for="item of props.list" name='item' v-bind="item"></slot>
+    <template v-if="$slots.default">
+      <slot></slot>
     </template>
 
-    <template v-else>
+    <template v-if="list">
       <li
         v-for="item of props.list"
         class="px-3 py-2 mr-2 rounded-lg"
--- src/widgets/film-list/film-list.vue
+++ src/widgets/film-list/film-list.vue
@@ -6,6 +6,7 @@ import { AppLoader, AppText, AppSelectList } from '~/shared/ui';
 const emit = defineEmits<{(e: 'selectFilm', film: any): void}>();
 const isFetching = ref<boolean>(true);
 const list = ref<any[]>([]);
+const elms = ref([]);
 let unWatch: WatchStopHandle;
 
 onBeforeMount(async () => {
@@ -18,23 +19,74 @@ onBeforeMount(async () => {
 });
 
 onUnmounted(() => unWatch());
+
+let dragingEl: HTMLElement | null = null;
+let dragData: any;
+let dragInd: undefined | number;
+
+const dragStart = (e: DragEvent, item: any, index: number) => {
+  const target = e.target as HTMLElement;
+  if (target.tagName === 'A') {
+    dragingEl = e.target as HTMLElement;
+    dragInd = index;
+    dragData = item;
+    dragingEl.classList.add('draging')
+  }
+}
+
+const dragEnd = async (e: DragEvent)=> {
+  if (dragingEl) dragingEl.classList.remove('draging')
+  dragData = undefined;
+  dragingEl = null;
+
+  isFetching.value = true;
+  const db = await DBAPI.instance();
+  db.data.list = list.value;
+  await db.save();
+  isFetching.value = false;
+}
+
+const dragEnter = (e: DragEvent, item: any, index: number) => {
+  const target = e.target as HTMLElement;
+  if ((target.tagName === 'A' && dragingEl) && !dragingEl.isEqualNode(target)) {
+    let newList = [...list.value];
+
+    if (typeof dragInd === 'number' && dragInd > index) {
+      newList.splice(index, 0, dragData);
+      newList.splice(dragInd+1, 1);
+    }
+    if (typeof dragInd === 'number' && dragInd < index) {
+      newList.splice(index+1, 0, dragData);
+      newList.splice(dragInd, 1);
+    }
+    
+    dragInd = index;
+    list.value = newList;
+  }
+}
 </script>
 
 <template>
-  <div v-if="isFetching" class="flex justify-center">
-    <AppLoader size="md" />
-  </div>
-  <AppSelectList class="h-[65vh]" :list="list" v-else>
-    <template #item="item">
-      <NuxtLink
-        :to="{ path: '/my-films', query: {id: item.id, type: item.type} }"
-        class="item text-[0.8rem] px-3 py-2 mr-2 rounded-lg flex"
-        :class="{active: Number($route.query.id) === Number(item.id)}"
-        @click="$emit('selectFilm', item)"
-      >
-        <AppText variant="simple">{{ item.title }}</AppText>
-      </NuxtLink>
-    </template>
+  <AppSelectList class="h-[65vh]">
+    <div v-if="isFetching" class="flex justify-center">
+      <AppLoader size="md" />
+    </div>
+    <NuxtLink
+      v-for="(item, index) in list"
+      :key="item.id"
+      ref="elms"
+      :to="{ path: '/my-films', query: {id: item.id, type: item.type} }"
+      class="item text-[0.8rem] px-3 py-2 mr-2 rounded-lg flex"
+      :class="{active: Number($route.query.id) === Number(item.id)}"
+      @click="$emit('selectFilm', item)"
+      draggable="true"
+      :data-index="index"
+      @dragstart="dragStart($event, item, index)"
+      @dragend="dragEnd"
+      @dragenter.capture="dragEnter($event, item, index)"
+    >
+      {{ item.title }}
+    </NuxtLink>
   </AppSelectList>
 </template>
 
@@ -42,6 +94,10 @@ onUnmounted(() => unWatch());
 .item {
   cursor: pointer;
   font-size: .8em;
+
+  &.draging {
+    background-color: rgba($color: #eee, $alpha: 0.2);
+  }
   
   &.active {
     background-color: rgba($color: #000, $alpha: 0.2);
