6b112c5d4c0fb6967e2d45cdeaf7b0b2bf8db5cb
timon <timonsus@gmail.com>
2025-05-02 15:50:21 +0500

    feature[img]: add component img-resize

---
+++ src/shared/ui/img-resize.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+type Props = {
+  src: string;
+  width?: string | number;
+}
+
+const props = defineProps<Props>();
+
+const hover = ref(false);
+</script>
+
+<template>
+  <div class="relative aspect-[2/3] object-cover" :style="{ width: `${width}px` }">
+    <div
+      class="absolute transition hover:z-10 hover:scale-150 hover:shadow-md"
+      @mouseenter="hover = true"
+      @mouseleave="hover = false"
+    >
+      <img :src="src" class="rounded-md" />
+    </div>
+  </div>
+</template>
