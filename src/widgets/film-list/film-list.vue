e31b4e4f33faeff48e343283edf73bf94660da5b
timon <timonsus@gmail.com>
2025-05-09 03:58:00 +0500

    feature[film-list]: add filter

--- src/widgets/film-list/film-list.vue
+++ src/widgets/film-list/film-list.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
 import { WatchStopHandle } from 'nuxt/dist/app/compat/capi';
-import { DBAPI } from '~/entities/film';
+import { DBAPI, MainDataFilm } from '~/entities/film';
 import { AppLoader, AppText, AppSelectList } from '~/shared/ui';
+import FilmListFilter from './film-list.filter.vue';
+import { FilterFields } from './types';
 
+const db = await DBAPI.instance();
 const emit = defineEmits<{(e: 'selectFilm', film: any): void}>();
 const isFetching = ref<boolean>(true);
 const list = ref<any[]>([]);
 let unWatch: WatchStopHandle;
 
+const filterState = ref<FilterFields>({
+  hideWatched: false,
+})
+
+const filter = (list: MainDataFilm[]) => {
+  return list.filter((el) => {
+    if (filterState.value.hideWatched) return (db.data.watched || []).indexOf(el.id) < 0;
+    return true;
+  })
+};
+
 onBeforeMount(async () => {
   const db = await DBAPI.instance();
   isFetching.value = db.isFetching.value;
 
-  unWatch = watch(() => db.data.list, (listData) => {
-    list.value = listData;
-  }, {immediate: true});
+  unWatch = watch(
+    () => db.data.list,
+    (listData) => list.value = filter(listData),
+    {immediate: true}
+  );
 });
+
+watch(filterState, (state) => list.value = filter(db.data.list));
 
 onUnmounted(() => unWatch());
 </script>
@@ -24,7 +42,12 @@ onUnmounted(() => unWatch());
   <div v-if="isFetching" class="flex justify-center">
     <AppLoader size="md" />
   </div>
+
   <AppSelectList class="h-[65vh]" :list="list" v-else>
+    <template #top>
+      <FilmListFilter v-model="filterState" />
+    </template>
+
     <template #item="item">
       <NuxtLink
         :to="{ path: '/my-films', query: {id: item.id, type: item.type} }"
