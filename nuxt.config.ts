6d8a48435feed274d8d374d6fdbc9e7979c2e005
timon <timonsus@gmail.com>
2025-06-14 16:44:20 +0500

    refactoring[youtube]: remove lib youtube component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true },
+  compatibilityDate: "2025-06-14",
   srcDir: './src/',
   pages: true,
   experimental: {
